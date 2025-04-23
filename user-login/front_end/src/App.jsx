6cf0c35cf541e8bcff3b0c90e92d886ebab38b6f
Layyana Junaid <142867946+layyana-junaid@users.noreply.github.com>
2025-04-23 14:43:24 +0500

    Add files via upload

---
+++ user-login/front_end/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import './App.css';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ user-login/front_end/src/components/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Signup() {
+  return (
+    <div className="container">
+      <h2>Sign Up</h2>
+      <p>Welcome! Join now and make booking easier.</p>
+      {/* Form to be implemented */}
+    </div>
+  );
+}
+
+export default Signup;
